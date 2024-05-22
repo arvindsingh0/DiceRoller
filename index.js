2208bc3989f3902ba429ec2ac9907c66f3269eb2
arvindsingh0 <arvinsingh90277@gmail.com>
2024-05-22 12:21:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function rollDice()
+{
+    const numofDice=document.getElementById("numofDice").value;
+    const diceResult=document.getElementById("diceResult");
+    const diceImage=document.getElementById("diceImage");
+    const values=[];
+    const images=[];
+    for( let i=0;i<numofDice;i++)
+    {
+        const value=Math.floor(Math.random()*6)+1;
+        values.push(value);
+        images.push(`<img src="dice_images/${value}.jpg">`);
+    }
+    diceResult.textContent=`dice: ${values.join(', ')}`;
+    diceImage.innerHTML= images.join(' ');
+}
